ec13e6e2df26458d29cc1e19541b0bfc05d9c060
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-08-12 22:17:24 -0400

    Done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, style_hash|
+    style_hash.each do |language, type_hash|
+      if new_hash.has_key?(language)
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = type_hash
+        new_hash[language][:style] = []
+        new_hash[language][:style] << style
+      end
+    end
+  end
+  new_hash
 end
